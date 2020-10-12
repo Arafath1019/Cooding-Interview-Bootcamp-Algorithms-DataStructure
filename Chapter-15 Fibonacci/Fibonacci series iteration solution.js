c08b8169569be1d1c8e9f4f89f00048d5196e860
Arafath1019 <arafath.yeasin1019@gmail.com>
2020-10-12 09:11:30 +0600

    Uplaoding fibonacci problem solution

---
+++ Chapter-15 Fibonacci/Fibonacci series iteration solution.js
@@ -0,0 +1,13 @@
+function fibonacci(n) {
+    const result = [0, 1];
+    
+    for (let i = 2; i <= n; i++){
+        let a = result[i - 1];
+        let b = result[i - 2];
+        result.push(a + b);
+    }
+
+    return result[n];
+}
+
+console.log(fibonacci(9));
